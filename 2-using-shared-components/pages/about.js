513c4530e1b9f7c541668944c854ceadae6f492f
Rafael Xavier <rafaelxavier@luby.com.br>
2019-10-24 18:48:03 -0300

    feat: finished the tutorials (1-7)

--- 2-using-shared-components/pages/about.js
+++ 2-using-shared-components/pages/about.js
@@ -1,7 +1,11 @@
+// pages/about.js
+
+import Layout from '../components/MyLayout';
+
 export default function About() {
   return (
-    <div>
+    <Layout>
       <p>This is the about page</p>
-    </div>
-  )
+    </Layout>
+  );
 }
